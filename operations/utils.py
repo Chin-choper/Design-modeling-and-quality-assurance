b360de1d56b3cf296737b9f8acba35d3eed45d20
Chin-choper <kirillpass47@gmail.com>
2025-12-25 13:40:47 +0100

    1 частина 2 лаб роботи

---
+++ operations/utils.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+def get_db_handle():
+    client = MongoClient('localhost', 27017)
+    db = client['ZaporizhzhiaTradeDB']
+    return db, client
+
+def get_collection_handle():
+    db, client = get_db_handle()
+    return db['trade_operations']
